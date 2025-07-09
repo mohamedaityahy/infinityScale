f0a25d56c38617e12a51947a3fa9248fa4dae1ed
mohamedaityahy <86058357+mohamedaityahy@users.noreply.github.com>
2025-07-09 17:47:53 +0100

    font familly error fixing

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
-import { Cairo } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const cairo = Cairo({
-  subsets: ["arabic"],
-  variable: "--font-cairo",
-  weight: ["400", "700"],
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -14,11 +18,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ar">
-      <head>
-        <link rel="icon" href="/icon.svg" />
-      </head>
-      <body className={cairo.variable}>
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
