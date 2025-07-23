bb95766aa6aeda12ee5d876fe58f1affa9243be4
Mohamed Aityahy <86058357+mohamedaityahy@users.noreply.github.com>
2025-07-23 09:43:57 +0000

    add  vercle Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppLayoutClient from "./AppLayoutClient";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
